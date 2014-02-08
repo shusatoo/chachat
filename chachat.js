823eab8947e5aaebf69a4d650b2007cb933e12e8
Shuji Sato <s.satoo@gmail.com>
2014-02-08 09:43:30 +0900

    implement simple chat index.html and socket.io installed.

--- chachat.js
+++ chachat.js
@@ -4,21 +4,25 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type':'text/html'});
-    var output = fs.reqdFileSync("./chachat.html", "utf-8");
+    var output = fs.readFileSync("./index.html", "utf-8");
     res.end(output);
 }).listen(process.env.VMC_APP_PORT || 3000);
 
+// サーバーとソケットの紐付け
 var io = socketio.listen(server);
 
+// クライアントからのアクション
 io.sockets.on('connection', function(socket) {
 
-    // メッセージ送信
+    // メッセージ送信（カスタムイベントの呼び出し）
     socket.on('msg_to_server', function(data) {
+        // 自分を含めて全員に送信
         io.sockets.emit('msg_to_client', {value: data.value});
     });
 
-    // ブロードキャスト
+    // ブロードキャスト（カスタムイベントの呼び出し）
     socket.on('bcast_to_server', function(data) {
+        // 自分以外の全員に送信
         socket.broadcast.emit('msg_to_client', {value: data.value});
     });
 
